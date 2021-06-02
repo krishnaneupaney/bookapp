9f6b69212ef023a50ce69ba506b6dc4d82759c88
krishnaneupaney <krishnaneupaney000@gmail.com>
2021-06-01 23:04:16 -0400

    make it better

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
-dotenv.config()
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit: '30mb', extended: true}))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
